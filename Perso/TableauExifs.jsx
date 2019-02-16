302db7eebebd6ffaac222bcab2bb00b772e6b0ba
Hugues <hugues@huguesfrancois.fr>
2019-02-16 18:15:00 +0100

    Create TableauExifs.jsx
    Création d'un tableau avec les codes Exifs

---
+++ Perso/TableauExifs.jsx
@@ -0,0 +1,524 @@
+﻿function CreationDuTableauDesTagsExif () {
+	var tableauDesTagsExif=new Array();
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Gamma";
+	objetExif.property="Gamma";
+	tableauDesTagsExif[42240]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Photographic Sensitivity";
+	objetExif.property="PhotographicSensitivity";
+	tableauDesTagsExif[34855]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Sensitivity Type";
+	objetExif.property="SensitivityType";
+	tableauDesTagsExif[34864]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Standard Output Sensitivity";
+	objetExif.property="StandardOutput-Sensitivity";
+	tableauDesTagsExif[34865]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Recommended Exposure Index";
+	objetExif.property="RecommendedExposureIndex";
+	tableauDesTagsExif[34866]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="ISO Speed";
+	objetExif.property="ISOSpeed";
+	tableauDesTagsExif[34867]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="ISO Speed Latitude yyy";
+	objetExif.property="ISOSpeedLatitudeyyy";
+	tableauDesTagsExif[34868]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="ISO Speed Latitude zzz";
+	objetExif.property="ISOSpeedLatitudezzz";
+	tableauDesTagsExif[34869]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Camera Owner Name";
+	objetExif.property="CameraOwnerName";
+	tableauDesTagsExif[42032]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Body Serial Number";
+	objetExif.property="BodySerialNumber";
+	tableauDesTagsExif[42033]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Lens Specification";
+	objetExif.property="LensSpecification";
+	tableauDesTagsExif[42034]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Lens Make";
+	objetExif.property="LensMake";
+	tableauDesTagsExif[42035]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Lens Model";
+	objetExif.property="LensModel";
+	tableauDesTagsExif[42036]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Lens Serial Number";
+	objetExif.property="LensSerialNumber";
+	tableauDesTagsExif[42037]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Interoperability Index";
+	objetExif.property="InteroperabilityIndex";
+	tableauDesTagsExif[1]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image width";
+	objetExif.property="ImageWidth";
+	tableauDesTagsExif[256]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image height";
+	objetExif.property="ImageHeight";
+	tableauDesTagsExif[257]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Number of bits per component";
+	objetExif.property="BitsPerSample";
+	tableauDesTagsExif[258]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Compression scheme";
+	objetExif.property="Compression";
+	tableauDesTagsExif[259]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Pixel composition";
+	objetExif.property="PhotometricInterpretation";
+	tableauDesTagsExif[262]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Orientation of image";
+	objetExif.property="Orientation";
+	tableauDesTagsExif[274]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Number of components";
+	objetExif.property="SamplesPerPixel";
+	tableauDesTagsExif[277]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image data arrangement";
+	objetExif.property="PlanarConfiguration";
+	tableauDesTagsExif[284]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Subsampling ratio of Y to C";
+	objetExif.property="YCbCrSubSampling";
+	tableauDesTagsExif[530]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Y and C positioning";
+	objetExif.property="YCbCrPositioning";
+	tableauDesTagsExif[531]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image resolution in width direction";
+	objetExif.property="XResolution";
+	tableauDesTagsExif[282]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image resolution in height direction";
+	objetExif.property="YResolution";
+	tableauDesTagsExif[283]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Unit of X and Y resolution";
+	objetExif.property="ResolutionUnit";
+	tableauDesTagsExif[296]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image data location";
+	objetExif.property="StripOffsets";
+	tableauDesTagsExif[273]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Number of rows per strip";
+	objetExif.property="RowsPerStrip";
+	tableauDesTagsExif[278]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Bytes per compressed strip";
+	objetExif.property="StripByteCounts";
+	tableauDesTagsExif[279]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Offset to JPEG SOI";
+	objetExif.property="JPEGInterchangeFormat";
+	tableauDesTagsExif[513]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Bytes of JPEG data";
+	objetExif.property="JPEGInterchangeFormatLength";
+	tableauDesTagsExif[514]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Transfer function";
+	objetExif.property="TransferFunction";
+	tableauDesTagsExif[301]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="White point chromaticity";
+	objetExif.property="WhitePoint";
+	tableauDesTagsExif[318]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Chromaticities of primaries";
+	objetExif.property="PrimaryChromaticities";
+	tableauDesTagsExif[319]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Color space transformation matrix coefficients";
+	objetExif.property="YCbCrCoefficients";
+	tableauDesTagsExif[529]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Pair of black and white reference values";
+	objetExif.property="ReferenceBlackWhite";
+	tableauDesTagsExif[532]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="File change date and time";
+	objetExif.property="DateTime";
+	tableauDesTagsExif[306]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image title";
+	objetExif.property="ImageDescription";
+	tableauDesTagsExif[270]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image input equipment manufacturer";
+	objetExif.property="Make";
+	tableauDesTagsExif[271]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image input equipment model";
+	objetExif.property="Model";
+	tableauDesTagsExif[272]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Software used";
+	objetExif.property="Software";
+	tableauDesTagsExif[305]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Person who created the image";
+	objetExif.property="Artist";
+	tableauDesTagsExif[315]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Copyright holder";
+	objetExif.property="Copyright";
+	tableauDesTagsExif[33432]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exif version";
+	objetExif.property="ExifVersion";
+	tableauDesTagsExif[36864]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Supported Flashpix version";
+	objetExif.property="FlashpixVersion";
+	tableauDesTagsExif[40960]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Color space information";
+	objetExif.property="ColorSpace";
+	tableauDesTagsExif[40961]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Meaning of each component";
+	objetExif.property="ComponentsConfiguration";
+	tableauDesTagsExif[37121]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Image compression mode";
+	objetExif.property="CompressedBitsPerPixel";
+	tableauDesTagsExif[37122]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Valid image width";
+	objetExif.property="PixelXDimension";
+	tableauDesTagsExif[40962]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Valid image height";
+	objetExif.property="PixelYDimension";
+	tableauDesTagsExif[40963]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Manufacturer notes";
+	objetExif.property="MakerNote";
+	tableauDesTagsExif[37500]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="User comments";
+	objetExif.property="UserComment";
+	tableauDesTagsExif[37510]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Related audio file";
+	objetExif.property="RelatedSoundFile";
+	tableauDesTagsExif[40964]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Date and time of original data generation";
+	objetExif.property="DateTimeOriginal";
+	tableauDesTagsExif[36867]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Date and time of digital data generation";
+	objetExif.property="DateTimeDigitized";
+	tableauDesTagsExif[36868]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="DateTime subseconds";
+	objetExif.property="SubSecTime";
+	tableauDesTagsExif[37520]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="DateTimeOriginal subseconds";
+	objetExif.property="SubSecTimeOriginal";
+	tableauDesTagsExif[37521]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="DateTimeDigitized subseconds";
+	objetExif.property="SubSecTimeDigitized";
+	tableauDesTagsExif[37522]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Unique image ID";
+	objetExif.property="ImageUniqueID";
+	tableauDesTagsExif[42016]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exposure time";
+	objetExif.property="ExposureTime";
+	tableauDesTagsExif[33434]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="F number";
+	objetExif.property="FNumber";
+	tableauDesTagsExif[33437]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exposure program";
+	objetExif.property="ExposureProgram";
+	tableauDesTagsExif[34850]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Spectral sensitivity";
+	objetExif.property="SpectralSensitivity";
+	tableauDesTagsExif[34852]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="ISO speed rating";
+	objetExif.property="ISOSpeedRatings";
+	tableauDesTagsExif[34855]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Optoelectric conversion factor";
+	objetExif.property="OECF";
+	tableauDesTagsExif[34856]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Shutter speed";
+	objetExif.property="ShutterSpeedValue";
+	tableauDesTagsExif[37377]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Aperture";
+	objetExif.property="ApertureValue";
+	tableauDesTagsExif[37378]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Brightness";
+	objetExif.property="BrightnessValue";
+	tableauDesTagsExif[37379]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exposure bias";
+	objetExif.property="ExposureBiasValue";
+	tableauDesTagsExif[37380]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Maximum lens aperture";
+	objetExif.property="MaxApertureValue";
+	tableauDesTagsExif[37381]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Subject distance";
+	objetExif.property="SubjectDistance";
+	tableauDesTagsExif[37382]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Metering mode";
+	objetExif.property="MeteringMode";
+	tableauDesTagsExif[37383]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Light source";
+	objetExif.property="LightSource";
+	tableauDesTagsExif[37384]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Flash";
+	objetExif.property="Flash";
+	tableauDesTagsExif[37385]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Lens focal length";
+	objetExif.property="FocalLength";
+	tableauDesTagsExif[37386]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Subject area";
+	objetExif.property="SubjectArea";
+	tableauDesTagsExif[37396]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Flash energy";
+	objetExif.property="FlashEnergy";
+	tableauDesTagsExif[41483]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Spatial frequency response";
+	objetExif.property="SpatialFrequencyResponse";
+	tableauDesTagsExif[41484]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Focal plane X resolution";
+	objetExif.property="FocalPlaneXResolution";
+	tableauDesTagsExif[41486]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Focal plane Y resolution";
+	objetExif.property="FocalPlaneYResolution";
+	tableauDesTagsExif[41487]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Focal plane resolution unit";
+	objetExif.property="FocalPlaneResolutionUnit";
+	tableauDesTagsExif[41488]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Subject location";
+	objetExif.property="SubjectLocation";
+	tableauDesTagsExif[41492]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exposure index";
+	objetExif.property="ExposureIndex";
+	tableauDesTagsExif[41493]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Sensing method";
+	objetExif.property="SensingMethod";
+	tableauDesTagsExif[41495]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="File source";
+	objetExif.property="FileSource";
+	tableauDesTagsExif[41728]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Scene type";
+	objetExif.property="SceneType";
+	tableauDesTagsExif[41729]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="CFA pattern";
+	objetExif.property="CFAPattern";
+	tableauDesTagsExif[41730]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Custom image processing";
+	objetExif.property="CustomRendered";
+	tableauDesTagsExif[41985]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Exposure mode";
+	objetExif.property="ExposureMode";
+	tableauDesTagsExif[41986]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="White balance";
+	objetExif.property="WhiteBalance";
+	tableauDesTagsExif[41987]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Digital zoom ratio";
+	objetExif.property="DigitalZoomRatio";
+	tableauDesTagsExif[41988]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Focal length in 35 mm film";
+	objetExif.property="FocalLengthIn35mmFilm";
+	tableauDesTagsExif[41989]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Scene capture type";
+	objetExif.property="SceneCaptureType";
+	tableauDesTagsExif[41990]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Gain control";
+	objetExif.property="GainControl";
+	tableauDesTagsExif[41991]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Contrast";
+	objetExif.property="Contrast";
+	tableauDesTagsExif[41992]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Saturation";
+	objetExif.property="Saturation";
+	tableauDesTagsExif[41993]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Sharpness";
+	objetExif.property="Sharpness";
+	tableauDesTagsExif[41994]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Device settings description";
+	objetExif.property="DeviceSettingDescription";
+	tableauDesTagsExif[41995]=objetExif;
+
+	var objetExif=new ObjetExif();
+	objetExif.label="Subject distance range";
+	objetExif.property="SubjectDistanceRange";
+	tableauDesTagsExif[41996]=objetExif;
+
+
+/**
+	var objetExif=new ObjetExif();
+	objetExif.label="";
+	objetExif.property="";
+	tableauDesTagsExif[]=objetExif;
+
+**/
+
+	return tableauDesTagsExif;
+}
+
+function ObjetExif () {
+	this.property;
+	this.label;
+}
